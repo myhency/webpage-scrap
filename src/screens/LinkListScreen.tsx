0dc2d1f06c1eaef0f74c669ff5113d076d5edfa1
myhency <hency.yeo@gmail.com>
2023-01-16 22:05:52 +0900

    feat: floating button 추가

--- src/screens/LinkListScreen.tsx
+++ src/screens/LinkListScreen.tsx
@@ -1,13 +1,16 @@
 import { useNavigation } from "@react-navigation/native";
 import { useCallback } from "react";
 import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Button } from "../components/Button";
 import { Header } from "../components/Header/Header";
+import { Icon } from "../components/Icons";
 import { Spacer } from "../components/Spacer";
 import { Typography } from "../components/Typography";
 
 export const LinkListScreen = () => {
     const navigation = useNavigation();
+    const safeAreaInset = useSafeAreaInsets();
     const onPressButton = useCallback(() => {
         navigation.navigate("LinkStack", { screen: "LinkDetail" });
     }, []);
@@ -28,6 +31,28 @@ export const LinkListScreen = () => {
                 <Spacer size={12} />
                 <Button onPress={onPressAddButton}>
                     <Typography fontSize={16}>Go to add link</Typography>
+                </Button>
+            </View>
+            <View
+                style={{
+                    position: "absolute",
+                    right: 24,
+                    bottom: 24 + safeAreaInset.bottom,
+                }}
+            >
+                <Button onPress={onPressAddButton}>
+                    <View
+                        style={{
+                            width: 52,
+                            height: 52,
+                            borderRadius: 26,
+                            alignItems: "center",
+                            justifyContent: "center",
+                            backgroundColor: "black",
+                        }}
+                    >
+                        <Icon name="add" size={32} color="white" />
+                    </View>
                 </Button>
             </View>
         </View>
