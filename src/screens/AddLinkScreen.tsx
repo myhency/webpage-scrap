c621ab1c6d8dba22ad326ecbab048c9d400481a9
myhency <hency.yeo@gmail.com>
2023-01-16 22:29:49 +0900

    feat: ui 추가

--- src/screens/AddLinkScreen.tsx
+++ src/screens/AddLinkScreen.tsx
@@ -1,10 +1,18 @@
 import { useNavigation } from "@react-navigation/native";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Button } from "../components/Button";
 import { Header } from "../components/Header/Header";
+import { SingleLineInput } from "../components/SingleLineInput";
+import { Spacer } from "../components/Spacer";
+import { Typography } from "../components/Typography";
 
 export const AddLinkScreen = () => {
     const navigation = useNavigation();
+    const insets = useSafeAreaInsets();
+    const [url, setUrl] = useState("");
+
     const onPressBack = useCallback(() => {
         navigation.goBack();
     }, []);
@@ -16,6 +24,39 @@ export const AddLinkScreen = () => {
                 </Header.Group>
                 <Header.Icon iconName="close" onPress={onPressBack} />
             </Header>
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    paddingHorizontal: 24,
+                }}
+            >
+                <SingleLineInput
+                    value={url}
+                    onChangeText={setUrl}
+                    placeholder="https://example.com"
+                    keyboardType="url"
+                />
+            </View>
+            <Button onPress={function (): void {}}>
+                <View
+                    style={{ backgroundColor: url === "" ? "gray" : "black" }}
+                >
+                    <View
+                        style={{
+                            height: 52,
+                            alignItems: "center",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <Typography color="white" fontSize={16}>
+                            Add
+                        </Typography>
+                    </View>
+                    <Spacer size={insets.bottom} />
+                </View>
+            </Button>
         </View>
     );
 };
