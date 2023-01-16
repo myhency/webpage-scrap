99ec842643f37245fc86d87e165b0a4275cd09bd
myhency <hency.yeo@gmail.com>
2023-01-16 23:13:27 +0900

    feat: url list 아톰 추가

--- src/states/atomLinkList.ts
+++ src/states/atomLinkList.ts
@@ -1,6 +1,13 @@
 import { atom } from "recoil";
 
-export const atomLinkList = atom<{ list: [] }>({
+export interface Link {
+    title: string;
+    image: string;
+    link: string;
+    createdAt: string;
+}
+
+export const atomLinkList = atom<{ list: Link[] }>({
     key: "MAIN/LINK_LIST",
     default: {
         list: [],
