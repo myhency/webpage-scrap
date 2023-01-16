197f042c9b67d65da4b91c2bbecf5d9b1d6cf232
myhency <hency.yeo@gmail.com>
2023-01-17 02:00:29 +0900

    feat: effects 추가 (https://cyclic-yard-58c.notion.site/React-Native-TIL_Recoil_effect_-_230117-39105c85f2c849c799746988b524f86b)

--- src/states/atomLinkList.ts
+++ src/states/atomLinkList.ts
@@ -1,4 +1,5 @@
-import { atom } from "recoil";
+import { atom, AtomEffect } from "recoil";
+import { getItem, removeItem, setItem } from "../utils/async-storage-utils";
 
 export interface Link {
     title: string;
@@ -7,9 +8,31 @@ export interface Link {
     createdAt: string;
 }
 
+const asyncStorageEffect =
+    (key: string): AtomEffect<{ list: Link[] }> =>
+    ({ setSelf, onSet, trigger }) => {
+        const loadPersisted = async () => {
+            const savedValue = await getItem(key);
+            if (savedValue !== null) {
+                setSelf(JSON.parse(savedValue));
+            }
+        };
+
+        if (trigger === "get") {
+            loadPersisted();
+        }
+
+        onSet((newValue, _, isReset: boolean) => {
+            console.debug("onSet", newValue);
+
+            isReset ? removeItem(key) : setItem(key, JSON.stringify(newValue));
+        });
+    };
+
 export const atomLinkList = atom<{ list: Link[] }>({
     key: "MAIN/LINK_LIST",
     default: {
-        list: [],
+        list: [] as Link[],
     },
+    effects: [asyncStorageEffect("MAINLINK_LIST")],
 });
